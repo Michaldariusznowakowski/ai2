a344b878c9a812a079aa65794b574679b466b61c
Michał Dariusz Nowakowski <Michaldariusznowakowski@protonmail.com>
2023-11-02 16:30:15 +0100

    labD

---
+++ pogodynka/src/Form/MeasurementType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Entity\Measurement;
+use App\Entity\Location;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class MeasurementType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('date', DateType::class, [
+                'widget' => 'single_text',
+                'html5' => true,
+            ])
+            ->add('calsius', NumberType::class, [
+                'attr' => [
+                    'min' => -20,
+                    'max' => 40,
+                ],
+                'html5' => true,
+            ])
+            ->add('location', EntityType::class, [
+                'class' => 'App\Entity\Location',
+                'choice_label' => 'city',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Measurement::class,
+        ]);
+    }
+}
